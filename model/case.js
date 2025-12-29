36c675e5af4de2becd58423e971204f75b54accf
Bhargav-Bhamare <bbhamare9e20@gmail.com>
2025-12-29 13:34:09 +0530

    Initializing Schema for Case Data

---
+++ model/case.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const caseSchema = new Schema({
+    caseType: { type: String, required: true },
+    courtType: { type: String, required: true },
+    caseNumber: { type: String, required: true },
+    petitioner: { type: String, required: true },
+    respondent: { type: String, required: true },
+    stage: { type: String, required: true },
+    nextHearingDate: { type: Date, required: true },
+    timeSlot: { type: String, required: true },
+    courtFee: { type: Number, required: false },
+    status: { type: String, required: true },
+    affidavitId: { type: Number, required: false },
+    vakalatnamaNumber : {type : Number, required : false}
+});
+
+const Case = mongoose.model("Case", caseSchema);
+module.exports = Case;
